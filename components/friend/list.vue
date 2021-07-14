8afef8a79107fbad1d4d8d07c790e9da06ceb4e5
phellipeperin <phellipe.perin@gmail.com>
2021-07-14 23:26:15 +0200

    friends page part4

--- components/friend/list.vue
+++ components/friend/list.vue
@@ -22,10 +22,23 @@ import User from '~/application/domain/user/user';
 
 export default Vue.extend({
   methods: {
-    remove(friend: User): void {
-      // TODO
-      // const newFriendList = this.$store.state.
-      // this.$store.commit('hero/SET_HERO', hero);
+    async remove(friend: User): Promise<void> {
+      try {
+        const newFriendList = this.$store.state.user.user.friends.filter(elem => elem !== friend.id);
+        const docRef = this.$fire.firestore.collection('users').doc(this.$store.state.user.user.id);
+        const data = {
+          friends: JSON.parse(JSON.stringify(newFriendList)),
+        };
+        await docRef.update(data);
+        this.$store.commit('user/SET_FRIENDS', newFriendList);
+
+        const newFriendDataList = this.$store.state.friend.list.filter(elem => elem.id !== friend.id);
+        this.$store.commit('friend/SET_LIST', newFriendDataList);
+
+        this.$store.commit('feedback/SHOW_SUCCESS_MESSAGE', 'Friend Removed Successfully');
+      } catch (e) {
+        this.$store.commit('feedback/SHOW_ERROR_MESSAGE', e);
+      }
     },
   },
 });
--- components/friend/list/item.vue
+++ components/friend/list/item.vue
@@ -1,32 +1,45 @@
 <template>
-  <v-sheet
-    shaped
-    :elevation="2"
-    height="140"
-    width="280"
-    class="ma-2 py-2 px-4 d-flex align-center"
-  >
-    <ui-avatar
-      :photo-url="friend.systemInfo.photoUrl"
-      size="82"
-      class="mr-6"
-    />
-    <div class="d-flex flex-column">
-      <span class="text-h6 mb-2">{{ friend.gameInfo.nickname }}</span>
-      <v-chip
-        small
-        class="mb-1"
-      >
-        Player <b class="ml-1">Lv. {{ friend.gameInfo.playerLevel }}</b>
-      </v-chip>
-      <v-chip
-        small
-        outlined
-      >
-        Crystal <b class="ml-1">Lv. {{ friend.gameInfo.crystalLevel }}</b>
-      </v-chip>
-    </div>
-  </v-sheet>
+  <v-hover v-slot="{ hover }">
+    <v-sheet
+      shaped
+      :elevation="2"
+      :height="hover ? 170 : 140"
+      width="280"
+      :class="`item ma-2 py-2 px-4 d-flex align-center ${hover ? 'item__hover' : ''}`"
+    >
+      <ui-avatar
+        :photo-url="friend.systemInfo.photoUrl"
+        size="82"
+        class="mr-6"
+      />
+      <div class="d-flex flex-column">
+        <span class="text-h6 mb-2">{{ friend.gameInfo.nickname }}</span>
+        <v-chip
+          small
+          class="mb-1"
+        >
+          Player <b class="ml-1">Lv. {{ friend.gameInfo.playerLevel }}</b>
+        </v-chip>
+        <v-chip
+          small
+          outlined
+        >
+          Crystal <b class="ml-1">Lv. {{ friend.gameInfo.crystalLevel }}</b>
+        </v-chip>
+
+        <v-btn
+          v-show="hover"
+          small
+          text
+          color="error"
+          class="mt-4"
+          @click="remove"
+        >
+          Remove
+        </v-btn>
+      </div>
+    </v-sheet>
+  </v-hover>
 </template>
 
 <script lang="ts">
@@ -46,5 +59,11 @@ export default Vue.extend({
 </script>
 
 <style scoped lang="scss">
+.item {
+  transition: all ease 0.3s;
 
+  &__hover {
+    transition: all ease 0.3s;
+  }
+}
 </style>
