9e24f1a343a8e3419ab936133fd5c136d5bbe969
phellipeperin <phellipe.perin@gmail.com>
2021-11-06 12:15:37 +0100

    equipment resources statistics

--- components/statistics/info/equipment.vue
+++ components/statistics/info/equipment.vue
@@ -1,7 +1,10 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12">
+      <v-col
+        cols="12"
+        sm="6"
+      >
         <ui-info-title text="Basic Info Needed">
           <span v-show="!!info.itemsNeeded"><b>{{ info.itemsNeeded }}</b> Items<br></span>
           <span><b>{{ info.starsNeeded }}</b> Stars<br></span>
@@ -9,6 +12,22 @@
           <span><b>{{ info.noFaction }}</b> Without Faction</span>
         </ui-info-title>
 
+        <div class="d-flex flex-wrap">
+          <ui-resource-quantity
+            :image-src="tokenImage"
+            :amount="info.tokensNeeded"
+          />
+          <ui-resource-quantity
+            :image-src="goldImage"
+            :amount="info.goldNeeded"
+          />
+        </div>
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="6"
+      >
         <ui-info-title text="Stones Needed">
           <b>{{ info.stonesNeeded.t1 + info.stonesNeeded.t2 + info.stonesNeeded.t3 }}</b> Total<br>
           - <b>{{ info.stonesNeeded.t1 }}</b> T1<br>
@@ -23,11 +42,20 @@
 <script lang="ts">
 import Vue from 'vue';
 import StatisticEquipmentInfo from '~/application/domain/statistic/info/statisticEquipmentInfo';
+import { loadEnhanceTokenRareImage, loadGoldImage } from '~/application/services/imageService';
 
 export default Vue.extend({
   props: {
     info: { type: StatisticEquipmentInfo, required: true },
     onCompare: { type: Boolean, required: false, default: false },
+  },
+  computed: {
+    goldImage() {
+      return loadGoldImage();
+    },
+    tokenImage() {
+      return loadEnhanceTokenRareImage();
+    },
   },
 });
 </script>
--- components/ui/resource-quantity.vue
+++ components/ui/resource-quantity.vue
@@ -25,6 +25,15 @@ export default Vue.extend({
   },
   methods: {
     formatNumber(value: number): string {
+      if (value >= 10000000) {
+        return `${this.addNumberSeparators(Math.round(value / 1000000))} M`;
+      }
+      if (value >= 1000000) {
+        return `${this.addNumberSeparators(Math.round(value / 1000))} K`;
+      }
+      return this.addNumberSeparators(value);
+    },
+    addNumberSeparators(value: number): string {
       return value.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, '.');
     },
   },
