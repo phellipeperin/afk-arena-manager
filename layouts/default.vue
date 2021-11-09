c3448356adc299bbfd347242ad54a1fe708cee68
phellipeperin <phellipe.perin@gmail.com>
2021-11-09 22:43:29 +0100

    add toolbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,51 @@
 <template>
   <v-app>
-    <div class="app-content">
-      <app-navigation-drawer />
+    <div class="overflow-hidden">
+      <v-app-bar
+        absolute
+        color="indigo darken-2"
+        dark
+        shrink-on-scroll
+        prominent
+        scroll-target="#content"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-main class="app-main">
-        <v-container
-          fluid
-          class="app-container px-5 py-4"
-        >
-          <Nuxt />
-          <app-snackbar />
-        </v-container>
-      </v-main>
+        <v-app-bar-title>Title</v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+<!--      <app-navigation-drawer />-->
+      <v-sheet
+        id="content"
+        class="base-content overflow-y-auto"
+      >
+        <div style="height: 1000px;">
+          <v-main>
+            <Nuxt />
+            <app-snackbar />
+          </v-main>
+        </div>
+      </v-sheet>
     </div>
   </v-app>
 </template>
 
 <style scoped lang="scss">
-.app-main,
-.app-container {
-  height: 100%;
+.base-content {
+  height: 100vh;
+  padding-top: 128px;
 }
 </style>
